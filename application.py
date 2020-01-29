afb7f86ea37e45110107015daeee442e0662afa4
ravinder564 <59814481+ravinder564@users.noreply.github.com>
2020-01-29 22:56:53 +0000

    added flask

--- application.py
+++ application.py
@@ -1,2 +1,11 @@
 from flask import Flask
 #from app import app
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hey its Python Flask application!'
+
+if __name__ == '__main__':
+  app.run()
